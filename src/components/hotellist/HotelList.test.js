e4d5376d91a678e3d881d58d7b8f07a58a6c15ef
ahtesham-quraish <ahtesham.quraish@gmail.com>
2018-09-11 21:38:06 +0500

    my hotel application

---
+++ src/components/hotellist/HotelList.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HotelList from './HotelList';
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import fetchHotelList from '../../../__mocks__/fetchHotels';
+Enzyme.configure({ adapter: new Adapter() });
+
+it('renders without crashing', async () => {
+  const hotelList = await fetchHotelList();  
+  const wrapper = shallow(<HotelList hotelList={hotelList.hotels} />);
+  expect(wrapper).toMatchSnapshot();
+});
+test('render a document name', async () => {
+    const hotelList = await fetchHotelList(); 
+    const wrapper = mount(
+        <HotelList hotelList={hotelList.hotels} />
+    );
+    expect(wrapper.prop('hotelList').length).toEqual(hotelList.hotels.length);
+});
+test('render a document name', async () => {
+    const hotelList = await fetchHotelList(); 
+    const wrapper = mount(
+        <HotelList hotelList={hotelList.hotels} />
+    );
+    expect(wrapper.find('li').length).toEqual(hotelList.hotels.length);
+});
+test('render a document name', async () => {
+    const hotelList = await fetchHotelList(); 
+    const wrapper = shallow(
+        <HotelList hotelList={hotelList.hotels} />
+    );
+    expect(wrapper.hasClass('card-list')).toEqual(true);
+});
