63b769e9b26567564289f0644d55d9c51b9d0b37
ahtesham-quraish <ahtesham.quraish@gmail.com>
2018-09-11 16:59:18 +0500

    my hotel application

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import './index.css';
 
 export default class Sidebar extends Component {
+    constructor(props) { 
+        super(props); 
+        this.state = { 
+            hotelName: '',
+            priceRange : 0
+        }; 
+    }
+  searchHotelNameStateHandler = (e) =>{
+    this.setState({hotelName : e.target.value});
+  }        
+  priceRangeHandler = () =>{
+
+  }
   render() {
+      const {hotelName, priceRange} = this.state;
     return (
         <aside id="sidebar">
             <div id="sidebar">
                 <form action="#" className="search-hotel">
-                    <input type="search" placeholder="Search Hotel"/>
-                    <input type="submit" value="" className="search-btn"/>
+                    <input type="search" value={hotelName} onChange={this.searchHotelNameStateHandler} placeholder="Search Hotel"/>
+                    <button className="search-btn">Submit</button>
                     <label htmlFor="price">Price Filter</label>
-                    <input type="range" id="price" name="cowbell" min="0" max="100" value="90" step="2"/>
+                    <input type="range" value={priceRange} onChange={this.priceRangeHandler} id="price" name="cowbell" min="0" max="100"  step="2"/>
                 </form>
             </div>
         </aside>
