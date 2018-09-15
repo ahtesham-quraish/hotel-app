fd8cffddc781a323e461b2fe341ee82d53c4d224
ahtesham-quraish <ahtesham.quraish@gmail.com>
2018-09-16 01:28:46 +0500

    WOP

--- src/components/sidebar/Sidebar.test.js
+++ src/components/sidebar/Sidebar.test.js
@@ -6,42 +6,44 @@ import Adapter from 'enzyme-adapter-react-16';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 Enzyme.configure({ adapter: new Adapter() });
-
+const Setup = () => {
+    return mount(<Sidebar />);
+}
 it('Simulate Input Change handler', () => {
   const event = {
     target: { value: 'A' },
   };
 
   let startDate = moment();
-  const wrapper = mount(<Sidebar />);
+  const wrapper = Setup();
   expect(wrapper.state('hotelName')).toBe('');
   wrapper.find('#searchText').simulate('change', event);
   expect(wrapper.state('hotelName')).toBe('A');
 });
 it('Check Input is mounted', () => {
-  const wrapper = mount(<Sidebar />);
+  const wrapper = Setup();
   expect(wrapper.find('input').length).toEqual(1);
 });
 
 it('Simulate Button click handler ', () => {
   const findHotelByName = jest.fn();
-  const props = {
-    findHotelByName,
-  };
-  const wrapper = mount(<Sidebar {...props} />);
+  const wrapper = Setup();
+  wrapper.setProps({findHotelByName : findHotelByName})
   const button = wrapper.find('button').first();
   button.simulate('click');
   expect(findHotelByName).toHaveBeenCalled();
 });
 it('Check setValue', () => {
   let startDate = moment();
-  const wrapper = mount(<Sidebar />);
+  const wrapper = Setup();
   expect(wrapper.state('value')).toBe(0);
   wrapper.instance().setValue(10);
   expect(wrapper.state('value')).toBe(10);
 });
 
 it('it simulate the form submission', () => {
-  const wrapper = mount(<Sidebar />);
+  const wrapper = Setup();
   wrapper.find('form').simulate('submit', { preventDefault: jest.fn() });
 });
+
+
